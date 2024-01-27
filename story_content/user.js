2ddd9f3247d1b40eef2dca7d7c3e3b43424680c4
Samantha Cortes Vudoyra <104947388+SamVudoyra@users.noreply.github.com>
2024-01-27 14:34:26 -0600

    Sumas JS

---
+++ story_content/user.js
@@ -0,0 +1,71 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6CB3chq9r98":
+        Script1();
+        break;
+      case "6ZlU2BNzRxG":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var randomnumber1 = Math.floor((Math.random()*99)+1);
+
+
+var randomnumber2 = Math.floor((Math.random()*99)+1);
+
+
+var player = GetPlayer();
+
+
+player.SetVar("numeroazar1",randomnumber1);
+
+
+player.SetVar("numeroazar2",randomnumber2);
+
+
+var x = player.GetVar("numeroazar1");
+
+
+var y = player.GetVar("numeroazar2");
+
+
+var z = x+y;
+
+
+player.SetVar("resultado",z);
+}
+
+function Script2()
+{
+  var randomnumber1 = Math.floor((Math.random()*99)+1);
+
+
+var randomnumber2 = Math.floor((Math.random()*99)+1);
+
+
+var player = GetPlayer();
+
+
+player.SetVar("numeroazar1",randomnumber1);
+
+
+player.SetVar("numeroazar2",randomnumber2);
+
+
+var x = player.GetVar("numeroazar1");
+
+
+var y = player.GetVar("numeroazar2");
+
+
+var z = x+y;
+
+
+player.SetVar("resultado",z);
+}
+
